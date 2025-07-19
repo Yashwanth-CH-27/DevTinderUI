e5854807bf34910f7bfff0b6ea476538931b7486
Yash <yaswanth.chundru@gmail.com>
2025-07-19 17:57:30 +0530

    Using DaisyUi

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,9 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Login";
+import Core from "./Core";
+import Layout from "./Layout";
 
 const Body = () => {
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element:<Layout/>,
+            children: [
+                {
+                    index: true,
+                    element: <Core/>,
+                },
+                {
+                    path:"login",
+                    element:<Login/>
+                },
+            ]
+        },
+    ])
     return(
-        <div>
-            <h1 className="font-bold text-4xl text-amber-700">Welcome!</h1>
-        </div>
+        <RouterProvider router={appRouter}/>
     )
 }
 
